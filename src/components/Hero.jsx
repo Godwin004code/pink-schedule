ee3f68e410de077cf8e6ce95c9044e4351edfc45
Tife <boluwatife1904@gmail.com>
2024-02-12 10:39:30 +0100

    added animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,54 +1,106 @@
-
-import React from 'react'
-import img from "../imagews/airbnb-host-welcoming-guests.jpg"
+import React, { useEffect } from "react";
+import img from "../imagews/airbnb-host-welcoming-guests.jpg";
+import gsap from "gsap";
 
 const Hero = () => {
-  return (
-    <section className='bg-black w-[100%] py-10'>
-        <section className='w-[90%] mx-auto flex justify-between'>
-        <div>
-            <div className='bg-white w-[] flex px-5 py-5 rounded-md mb-10'>
-            <svg className='w-[80px] h-[80px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
-            <div className='ml-6 my-auto'>
-                <h2 className='uppercase font-bold text-center'>Search</h2>
-                <p>
-                    Filter from area, distance, price and style
-                </p>
-            </div>
-            </div>
-            
-            <div className='bg-white w-[] flex px-5 py-5 rounded-md mb-10'>
-            <svg className='w-[80px] h-[80px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M75 75L41 41C25.9 25.9 0 36.6 0 57.9V168c0 13.3 10.7 24 24 24H134.1c21.4 0 32.1-25.9 17-41l-30.8-30.8C155 85.5 203 64 256 64c106 0 192 86 192 192s-86 192-192 192c-40.8 0-78.6-12.7-109.7-34.4c-14.5-10.1-34.4-6.6-44.6 7.9s-6.6 34.4 7.9 44.6C151.2 495 201.7 512 256 512c141.4 0 256-114.6 256-256S397.4 0 256 0C185.3 0 121.3 28.7 75 75zm181 53c-13.3 0-24 10.7-24 24V256c0 6.4 2.5 12.5 7 17l72 72c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-65-65V152c0-13.3-10.7-24-24-24z"/></svg>
-            <div className='ml-6 my-auto'>
-                <h2 className='uppercase font-bold text-center'>availability</h2>
-                <p>
-                    Find appointments on for the date and time you want
-                </p>
-            </div>
-            </div>
-            <div className='bg-white w-[] flex px-5 py-5 rounded-md mb-10'>
-            <svg className='w-[80px] h-[80px]' fill='pink' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
-            <div className='ml-6 my-auto'>
-                <h2 className='uppercase font-bold text-center'>for you</h2>
-                <p>
-                    Find stylists who can cater you you
-                </p>
-            </div>
-            </div>
-        </div>
-        <div className='w-[50%]'>
-            <div className='flex w-[100%]'>
-                <img  className="rounded-full w-[40%] h-[300px]" src={img} alt="img" />
-                 <img className="rounded-full w-[40%] h-[300px]"  src={img} alt="img" />
-            </div>
-            <div className='flex w-[100%]'>
-                <img  className="rounded-full w-[40%] h-[300px]" src={img} alt="img" />
-                 <img className="rounded-full w-[40%] h-[300px]"  src={img} alt="img" />
-            </div>
-        </div>
-        </section>
-    </section>
-  )
-}
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.from(".hero-image-bubble", {
+        opacity: 0,
+        duration: 2,
+      });
 
-export default Hero
+      gsap.from(".image-bubble-3", {
+        y: 100,
+        duration: 2,
+      });
+
+      gsap.from(".hero-rectangles", {
+        scale: 0.7,
+        duration: 2,
+      });
+    });
+
+    return () => {
+      ctx.revert();
+    };
+  }, []);
+
+  return (
+    <section className="bg-black w-[100%] py-10">
+      <section className="w-[90%] mx-auto flex justify-between">
+        <div>
+          <div className="bg-white w-[] flex px-5 py-5 rounded-md mb-10 hero-rectangles">
+            <svg
+              className="w-[80px] h-[80px]"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+            </svg>
+            <div className="ml-6 my-auto">
+              <h2 className="uppercase font-bold text-center">Search</h2>
+              <p>Filter from area, distance, price and style</p>
+            </div>
+          </div>
+
+          <div className="bg-white w-[] flex px-5 py-5 rounded-md mb-10 hero-rectangles">
+            <svg
+              className="w-[80px] h-[80px]"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path d="M75 75L41 41C25.9 25.9 0 36.6 0 57.9V168c0 13.3 10.7 24 24 24H134.1c21.4 0 32.1-25.9 17-41l-30.8-30.8C155 85.5 203 64 256 64c106 0 192 86 192 192s-86 192-192 192c-40.8 0-78.6-12.7-109.7-34.4c-14.5-10.1-34.4-6.6-44.6 7.9s-6.6 34.4 7.9 44.6C151.2 495 201.7 512 256 512c141.4 0 256-114.6 256-256S397.4 0 256 0C185.3 0 121.3 28.7 75 75zm181 53c-13.3 0-24 10.7-24 24V256c0 6.4 2.5 12.5 7 17l72 72c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-65-65V152c0-13.3-10.7-24-24-24z" />
+            </svg>
+            <div className="ml-6 my-auto">
+              <h2 className="uppercase font-bold text-center">availability</h2>
+              <p>Find appointments on for the date and time you want</p>
+            </div>
+          </div>
+          <div className="bg-white w-[] flex px-5 py-5 rounded-md mb-10 hero-rectangles">
+            <svg
+              className="w-[80px] h-[80px]"
+              fill="pink"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+            </svg>
+            <div className="ml-6 my-auto">
+              <h2 className="uppercase font-bold text-center">for you</h2>
+              <p>Find stylists who can cater you you</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-[50%]">
+          <div className="flex w-[100%]">
+            <img
+              className="hero-image-bubble rounded-full w-[40%] h-[300px]"
+              src={img}
+              alt="img"
+            />
+            <img
+              className="hero-image-bubble rounded-full w-[40%] h-[300px]"
+              src={img}
+              alt="img"
+            />
+          </div>
+          <div className="flex w-[100%]">
+            <img
+              className="hero-image-bubble image-bubble-3 rounded-full w-[40%] h-[300px]"
+              src={img}
+              alt="img"
+            />
+            <img
+              className="hero-image-bubble rounded-full w-[40%] h-[300px]"
+              src={img}
+              alt="img"
+            />
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,69 @@
-
-
-import React from 'react'
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const Search = () => {
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      function animProperties() {
+        return {
+          y: -50,
+          opacity: 0,
+        };
+      }
+
+      ScrollTrigger.create({
+        trigger: ".explore-section",
+        start: "bottom 80%",
+        animation: gsap
+          .timeline()
+          .from(".search-white-cont", {
+            y: -60,
+            opacity: 0,
+          })
+          .from(".search-heading-cont", animProperties())
+          .from(".search-bubble", animProperties())
+          .from(".search-btn", animProperties())
+          .from(".search-para", animProperties()),
+      });
+    });
+
+    return () => {
+      ctx.revert();
+    };
+  }, []);
   return (
-    <section className='bg-black w-[100%] flex flex-col justify-center items-center pb-16'>
-        
-        <section className='bg-white w-[60%] mx-auto py-8 rounded-2xl pb-28' >
-        <h2 className='uppercase text-4xl text-black font-bold text-center '>start</h2>
-        <h2 className='uppercase text-4xl text-black font-bold text-center '>search</h2>
-        <div className='flex justify-between w-[80%] mt-10 mx-auto'>
-        <div className='explore-box uppercase text-center text-4xl text-black font-bold bg-[#ccc] px-5 rounded-3xl py-3'>hair</div>
-        <div className='explore-box uppercase text-center text-4xl text-black font-bold bg-[#ccc] px-5 rounded-3xl py-3'>face</div>
-        <div className='explore-box uppercase text-center text-4xl text-black font-bold bg-[#ccc] px-5 rounded-3xl py-3'>body</div>
+    <section className="bg-black w-[100%] flex flex-col justify-center items-center pb-16">
+      <section className="bg-white w-[60%] mx-auto py-8 rounded-2xl pb-28 search-white-cont">
+        <div className="search-heading-cont">
+          <h2 className="uppercase text-4xl text-black font-bold text-center ">
+            start
+          </h2>
+          <h2 className="uppercase text-4xl text-black font-bold text-center ">
+            search
+          </h2>
         </div>
-
-        </section>
-        <button className='mt-7 uppercase bg-red-800 text-white font-medium py-3 px-7 mx-auto text-center rounded-2xl'>sign up</button>
-        <p className='text-white font-medium mt-4'>Create an account to personalise your search results</p>
+        <div className="flex justify-between w-[80%] mt-10 mx-auto">
+          <div className="uppercase text-center text-4xl text-black font-bold bg-[#ccc] px-5 rounded-3xl py-3 search-bubble">
+            hair
+          </div>
+          <div className="uppercase text-center text-4xl text-black font-bold bg-[#ccc] px-5 rounded-3xl py-3 search-bubble">
+            face
+          </div>
+          <div className="uppercase text-center text-4xl text-black font-bold bg-[#ccc] px-5 rounded-3xl py-3 search-bubble">
+            body
+          </div>
+        </div>
+      </section>
+      <button className="mt-7 uppercase bg-red-800 text-white font-medium py-3 px-7 mx-auto text-center rounded-2xl search-btn">
+        sign up
+      </button>
+      <p className="text-white font-medium mt-4 search-para">
+        Create an account to personalise your search results
+      </p>
     </section>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
